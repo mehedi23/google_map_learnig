93be9f49cf85e04f688a3d1ec929639e3ee72f28
mehedi23 <satusatusatu0@gmail.com>
2021-11-07 16:24:04 +0600

    get distance by pining

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var map;
 var geocoder;
 var click_1st = null;
 var click_2nd = null;
+var directionsService;
+var directionsRenderer;
+var all_custom_markers = []
+
+var action_btn = document.querySelector('.action-button');
+var first_input = document.getElementById("first_location");
+var travel_model = document.getElementById('travel_model_val').value;
+var first_location = document.getElementById("first_location");
+var second_location = document.getElementById("second_location");
 
 
 function initMap() {
@@ -15,137 +24,128 @@ function initMap() {
     });
 
 
-
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer({
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer({
         draggable: true,
         map
     });
 
-
     directionsRenderer.setMap(map);
 
-    const onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    const onChangeHandler = function (e , click_val) {
+        click_val = e.target.value
+
+        // console.log("click_1st", click_val)
+        // calculateAndDisplayRoute(directionsService, directionsRenderer);
     };
 
 
-    document.getElementById("first_location").addEventListener("change", onChangeHandler);
-    document.getElementById("second_input").addEventListener("change", onChangeHandler);
+    first_location.addEventListener("change" ,  e => onChangeHandler(e, click_1st));
+    second_location.addEventListener("change" , e => onChangeHandler(e, click_2nd));
+
 
     search_box();
-    // on_click_marker();
 
+
+    let infoWindow = new google.maps.InfoWindow();
+    infoWindow.open(map);
 
     geocoder = new google.maps.Geocoder();
 
-    // Create the initial InfoWindow.
-    let infoWindow = new google.maps.InfoWindow();
+    document.getElementById('travel_model_val').addEventListener("change", travel_model_selector);
 
-    infoWindow.open(map);
+    const labels = "AB"
 
     map.addListener("click", (e) => {
-        geocode_address(e.latLng);
 
-        // console.log( "1", click_2nd)
-        // console.log( "2", click_1st)
-
-
-        if (click_1st) {
-            // console.log("now go")
-            console.log("1", click_2nd)
-            calculateAndDisplayRoute(directionsService, directionsRenderer);
-        }
+        if(all_custom_markers.length < 2){
+            const single_markers =  new google.maps.Marker({
+                                        position: e.latLng,
+                                        map: map,
+                                        zoom: 14,
+                                        draggable: true,
+                                        clickable: true,
+                                        label: !click_1st ? labels[0] : labels[1],
+                                    });
+    
+            all_custom_markers.push(single_markers);
+        };
+        
+        !click_2nd && geocode_recording(e.latLng);
     });
-}
+
+    function travel_model_selector() {
+        travel_model = this.value;
+        click_1st && click_2nd && calculateAndDisplayRoute(directionsService, directionsRenderer, click_1st, click_2nd);
+    };
 
 
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+};
+
+
+
+
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer, first, second) {
+
+
     directionsService
         .route({
             origin: {
-                query: click_1st,
+                query: first,
             },
             destination: {
-                query: click_2nd,
+                query: second,
             },
-            travelMode: google.maps.TravelMode.DRIVING,
+            travelMode: google.maps.TravelMode[travel_model],
         })
         .then((response) => {
+            all_custom_markers[0].setMap(null);
+            all_custom_markers[1].setMap(null);
             directionsRenderer.setDirections(response);
         })
-        .catch((e) => console.log("Directions request failed due to " + e));
-}
+        .catch((e) =>  {
+            all_custom_markers[1].setMap(null);
+            all_custom_markers.splice(1, 1);
+            click_2nd = null;
+            console.log("Directions request failed due to " + e)
+        });
+};
 
 
 
 function search_box() {
-    const first__input = document.getElementById("first_location");
-    const second__input = document.getElementById("second_input");
-
-    new google.maps.places.SearchBox(first__input);
-    new google.maps.places.SearchBox(second__input);
-
-    // map.addListener("bounds_changed", () => {
-    //     searchBox.setBounds(map.getBounds());
-    // });
-
-    // searchBox.addListener("places_changed", () => {
-    //     const places = searchBox.getPlaces();
-    // })
-}
+    new google.maps.places.SearchBox(first_location);
+    new google.maps.places.SearchBox(second_location);
+};
 
 
 
-function geocode_address(lat_lng) {
+function geocode_recording(latLng) {
     geocoder
         .geocode({
-            location: lat_lng
+            location: latLng
         })
         .then((response) => {
             if (!click_1st) {
-                // console.log("firs")
-                click_1st = response.results[0].formatted_address
+                click_1st = response.results[0].formatted_address;
+                first_location.value = response.results[0].formatted_address;
             } else {
-                // console.log(click_1st)
-                click_2nd = response.results[0].formatted_address
+                click_2nd = response.results[0].formatted_address;
+                second_location.value = response.results[0].formatted_address;
+                calculateAndDisplayRoute(directionsService, directionsRenderer, click_1st, click_2nd);
             };
 
         })
         .catch((e) => {
             console.log(e)
-        })
-};
-
-var marker
-var marker_item = []
-
-function on_click_marker() {
-    google.maps.event.addListener(map, 'click', function (e) {
-
-        placeMarker(e.latLng);
-
-        const lat_lng = {
-            lat: parseFloat(e.latLng.lat()),
-            lng: parseFloat(e.latLng.lng()),
-        };
-
-        car_lat_lan = lat_lng
-        // geocode_address(lat_lng);
-    });
-}
-
-function placeMarker(location) {
-    if (marker_item.length < 2) {
-        marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true,
-            clickable: true
         });
-    } else {
-        marker.setPosition(location);
-    }
-    marker_item.push(marker);
 };
+
+
+
+action_btn.addEventListener("click", function () {
+    click_1st && click_2nd && calculateAndDisplayRoute(directionsService, directionsRenderer, click_1st, click_2nd);
+});
+
